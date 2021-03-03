adf64eb88c0d79f61b36145b74a85f6e37c6d36d
gzakhar <soccerzakharov@gmail.com>
2021-03-03 15:20:56 -0500

    todo

--- lemonke-redistricting-system-frontend/src/UI/Map/Map.js
+++ lemonke-redistricting-system-frontend/src/UI/Map/Map.js
@@ -25,6 +25,9 @@ function Map() {
 		map.on('load', () => {
 			map.addSource('state-data', {
 				type: 'geojson',
+				// Only the data in this project is available to the code here.
+				// All data therefor should be loaded into the "public" directory in this project.
+				// Later we will change this to be an api call.
 				data: './1.geojson'
 			});
 
@@ -32,8 +35,7 @@ function Map() {
 				id: 'state-data',
 				type: 'fill',
 				source: 'state-data',
-				layout: {
-				},
+				layout: {},
 				paint: {
 					'fill-color': '#088',
 					'fill-opacity': 0.8
@@ -46,6 +48,24 @@ function Map() {
 		map.addControl(new mapboxgl.NavigationControl(), 'bottom-right');
 
 	}, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+
+	// TODO:
+	// Add all districts onto the map
+	// Probably running thought a loop and "addLayer" for each of the distr.
+	const populatingLayers = () => {
+
+	}
+
+	// TODO:
+	// Random color generator, but somehow making sure that colors dont repeat.
+	// It might be hard to do this the right way,
+	// so for now just a function that spits out significatly different colors should be good enough.
+	// In the future this should probably be its own class that assigns color to a "district" Object.
+	// https://www.geeksforgeeks.org/graph-coloring-applications/#:~:text=Graph%20coloring%20problem%20is%20to,are%20colored%20using%20same%20color.
+	const addColor = () => {
+
+	}
 
 
 	return (
