2e95d6af1188972529b9b453fbc6f214a6e04156
gzakhar <soccerzakharov@gmail.com>
2021-03-03 14:26:19 -0500

    district layer

--- lemonke-redistricting-system-frontend/src/UI/Map/Map.js
+++ lemonke-redistricting-system-frontend/src/UI/Map/Map.js
@@ -7,22 +7,44 @@ mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 function Map() {
 
 	const mapContainer = useRef();
-	const [lng, setLng] = useState(-70.9);
-	const [lat, setLat] = useState(42.35);
+	const [lng, setLng] = useState(-72.9);
+	const [lat, setLat] = useState(41.0);
 	const [zoom, setZoom] = useState(9);
+	let inialized = 0;
 
-	useEffect(() => {
-		const map = new mapboxgl.Map({
+
+	useEffect(async () => {
+
+		let map = new mapboxgl.Map({
 			container: mapContainer.current,
 			style: 'mapbox://styles/mapbox/streets-v11',
 			center: [lng, lat],
 			zoom: zoom
 		});
+
+		map.on('load', () => {
+			map.addSource('state-data', {
+				type: 'geojson',
+				data: './1.geojson'
+			});
+
+			map.addLayer({
+				id: 'state-data',
+				type: 'fill',
+				source: 'state-data',
+				layout: {
+				},
+				paint: {
+					'fill-color': '#088',
+					'fill-opacity': 0.8
+				}
+			});
+
+		})
+
 		// add navigation control (the +/- zoom buttons)
 		map.addControl(new mapboxgl.NavigationControl(), 'bottom-right');
-	
 
-		return () => map.remove();
 	}, []); // eslint-disable-line react-hooks/exhaustive-deps
 
 
