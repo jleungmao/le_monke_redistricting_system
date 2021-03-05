92fab70ded29dc5dca8e7265ab30df397435cbb6
ema0013 <ema0013@bths.edu>
2021-03-05 00:00:22 -0500

    from geojson added district layers

--- lemonke-redistricting-system-frontend/src/UI/Map/Map.js
+++ lemonke-redistricting-system-frontend/src/UI/Map/Map.js
@@ -10,7 +10,7 @@ function Map() {
 	const [lng, setLng] = useState(-72.9);
 	const [lat, setLat] = useState(41.0);
 	const [zoom, setZoom] = useState(9);
-	let inialized = 0;
+	let stateData = null;
 
 
 	useEffect(async () => {
@@ -22,14 +22,20 @@ function Map() {
 			zoom: zoom
 		});
 
+		
+
+		loadJSONFile(function(response) {
+			stateData = JSON.parse(response);
+		 });
+
 		map.on('load', () => {
-			map.addSource('state-data', {
-				type: 'geojson',
-				// Only the data in this project is available to the code here.
-				// All data therefor should be loaded into the "public" directory in this project.
-				// Later we will change this to be an api call.
-				data: './2012_Congress.geojson'
-			});
+			// map.addSource('state-data', {
+			// 	type: 'geojson',
+			// 	// Only the data in this project is available to the code here.
+			// 	// All data therefor should be loaded into the "public" directory in this project.
+			// 	// Later we will change this to be an api call.
+			// 	data: './2012_Congress.geojson'
+			// });
 
 			// map.addLayer({
 			// 	id: 'state-data',
@@ -41,7 +47,7 @@ function Map() {
 			// 		'fill-opacity': 0.8
 			// 	}
 			// });
-			populatingLayers(map);
+			populatingLayers(map, stateData);
 		})
 
 		
@@ -55,24 +61,46 @@ function Map() {
 	// TODO:
 	// Add all districts onto the map
 	// Probably running thought a loop and "addLayer" for each of the distr.
-	const populatingLayers = (map) => {
-		let enacted = map.getSource('state-data');
-		console.log(enacted._data);
-		//data is not actually the json within, its the path to the geojson file
+	const populatingLayers = (map, stateData) => {
 		let i = 0;
-		for(feature in enacted._data){
-			i++;
-			this.map.addLayer({
-				id:i,
+		for(var feature of stateData.features){
+			console.log(feature);
+			map.addSource(
+				i.toString(),
+				{type: "geojson",
+				data: feature}
+			);
+
+			map.addLayer({
+				id: "district" + i.toString(),
 				type:'fill',
-				source:feature,
+				source: i.toString(),
 				paint: {
-					'fill-color': Math.floor(Math.random()*16777215).toString(16),
+					'fill-color': addColor(),
 					'fill-opacity': 0.8
 				}
 			})
+			i++;
 		}
 	}
+
+	function loadJSONFile(callback) {   
+
+		var xmlobj = new XMLHttpRequest();
+	
+		xmlobj.overrideMimeType("application/json");
+	
+		xmlobj.open('GET', './2012_Congress.geojson', true); // Provide complete path to your json file here. Change true to false for synchronous loading.
+	
+		xmlobj.onreadystatechange = function () {
+			  if (xmlobj.readyState == 4 && xmlobj.status == "200") {
+				// Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+				callback(xmlobj.responseText);
+			  }
+		};
+	
+		xmlobj.send(null);  
+	 }
 
 	// TODO:
 	// Random color generator, but somehow making sure that colors dont repeat.
@@ -81,7 +109,8 @@ function Map() {
 	// In the future this should probably be its own class that assigns color to a "district" Object.
 	// https://www.geeksforgeeks.org/graph-coloring-applications/#:~:text=Graph%20coloring%20problem%20is%20to,are%20colored%20using%20same%20color.
 	const addColor = () => {
-
+		var color = '#'+Math.floor(Math.random()*16777215).toString(16);
+		return color;
 	}
 
 
