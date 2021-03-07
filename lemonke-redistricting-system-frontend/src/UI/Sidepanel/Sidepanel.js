a252fa7f3eac4991714e5472186f9d556925fcbe
MintHana <minthana1137@gmail.com>
2021-03-07 01:43:59 -0500

    Added boxplot

--- lemonke-redistricting-system-frontend/src/UI/Sidepanel/Sidepanel.js
+++ lemonke-redistricting-system-frontend/src/UI/Sidepanel/Sidepanel.js
@@ -7,7 +7,8 @@ import MenuItem from '@material-ui/core/MenuItem';
 import Container from '@material-ui/core/Container';
 import ProgressBar from '../ProgressBar/ProgressBar';
 import Button from '@material-ui/core/Button';
-import Plot from 'react-plotly.js';
+import Boxplot from '../../components/Boxplot';
+import {Route} from 'react-router-dom';
 
 function Sidepanel(props) {
 
@@ -29,102 +30,6 @@ function Sidepanel(props) {
 			<>{value === index && (<h1>{children}</h1>)}</>
 		)
 	}
-	var xData = ['x1', 'x2',
-	'x3', 'x4',];
-
-
-	function getrandom(num , mul) {
-		var value = [ ];
-			for ( i = 0; i <= num; i++ ) {
-				var rand = Math.random() * mul;
-				value.push(rand);
-			}
-		return value;
-	}
-
-	var yData = [
-		getrandom(30 ,10),
-		getrandom(30, 20),
-		getrandom(30, 25),
-		getrandom(30, 40),
-	];
-
-	var colors = ['rgba(93, 164, 214, 0.5)', 'rgba(255, 144, 14, 0.5)', 'rgba(44, 160, 101, 0.5)', 'rgba(255, 65, 54, 0.5)'];
-
-	var data = [];
-
-	for ( var i = 0; i < xData.length; i ++ ) {
-		var result = {
-			type: 'box',
-			y: yData[i],
-			name: xData[i],
-			boxpoints: 'all',
-			jitter: 0.5,
-			whiskerwidth: 0.2,
-			fillcolor: colors[i],
-			marker: {
-				size: 2
-			},
-			line: {
-				width: 1
-			}
-		};
-	data.push(result);
-	};
-
-	var layout = {
-		title: 'title goes here',
-		yaxis: {
-			autorange: true,
-			showgrid: true,
-			zeroline: true,
-			dtick: 5,
-			gridcolor: 'rgb(255, 255, 255)',
-			gridwidth: 1,
-			zerolinecolor: 'rgb(255, 255, 255)',
-			zerolinewidth: 2
-		},
-		margin: {
-			l: 40,
-			r: 30,
-			b: 80,
-			t: 100
-		},
-		paper_bgcolor: 'rgb(255, 255, 255)',
-		plot_bgcolor: 'rgb(243, 243, 243)',
-		showlegend: false
-	};
-
-	var data1 = [
-		{
-			y: [0, 1, 1, 2, 3, 5, 8, 13, 21],
-			boxpoints: 'all',
-			jitter: 0.3,
-			pointpos: -1.8,
-			type: 'box'
-		},
-		{
-			x: ['trace 0'],
-			y: [18],
-			name: 'special marker',
-			text: 'special marker it is',
-			marker: {
-			size: 20
-			}
-		}
-	];
-
-	var marker = {x: ['x1'],
-				y: [3],
-				name: 'special point',
-				text: 'this is special point',
-				marker: {
-					size: 3
-				}
-	}
-	data.push(marker);
-
-
 
 
 	return (
@@ -184,9 +89,6 @@ function Sidepanel(props) {
 			<TabPanel value={activeStep} index={2}>
 				<Container maxWidth="sm">
 					<h5>View Filtered Districts, using the objective function.</h5>
-					<Plot data={data1} layout= {{width: 300, height: 300, title: 'title goes here'}}></Plot>
-	  			<Plot data={data} layout= {layout}></Plot>
-  				<Plot data={data1} layout= {{width: 300, height: 300, title: 'another title goes here'}}></Plot>
 				</Container>
 			</TabPanel>
 			<TabPanel value={activeStep} index={3}>
@@ -247,6 +149,7 @@ function Sidepanel(props) {
 						max={0.0000001}
 						valueLabelDisplay="auto"
 					/>
+					<Route exact path='/' component={Boxplot}></Route>
 				</Container>
 			</TabPanel>
 			{/* Button Next, Back and Finish  */}
