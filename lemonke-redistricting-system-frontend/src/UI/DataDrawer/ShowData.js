8f6b20fedddb60e8c4284844a652f04d71ffb32c
gzakhar <soccerzakharov@gmail.com>
2021-05-19 16:42:16 -0400

    finv1

--- lemonke-redistricting-system-frontend/src/UI/DataDrawer/ShowData.js
+++ lemonke-redistricting-system-frontend/src/UI/DataDrawer/ShowData.js
@@ -150,7 +150,7 @@ function ShowData(props) {
                             </TableRow>
                         </TableHead>
                         <TableBody>
-                            {container}
+                            {/* {container} */}
                         </TableBody>
                     </Table>
                 </TableContainer>
